fc4092313f342ff9ff2e127dd328e9a9c9902a71
Joel Francisco <joel.francisco@zenn.pt>
2017-05-26 03:28:10 +0100

    Scenes mockup

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour
+{
+
+	public void LoadLevel (string name)
+	{
+		Debug.Log ("Load level requested: " + name);
+		Application.LoadLevel (name);
+	}
+	
+	public void RequestQuit ()
+	{
+		Debug.Log ("Quit game requested");
+		Application.Quit ();
+	}
+	
+}
